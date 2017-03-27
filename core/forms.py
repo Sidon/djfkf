9ecb25a4cd1fe181c88a4fb75807555ab77fed0c
sidon <sidoncd@gmail.com>
2017-03-27 17:18:41 -0300

    Third commit

--- core/forms.py
+++ core/forms.py
@@ -1,19 +1,19 @@
 import json
 from django import forms
-from . import models as mdl
+from .models import *
 
 class RegCarForm(forms.ModelForm):
 
     dcars = {}
     list_cars = []
-    for car in mdl.Car.objects.all():
+    for car in Car.objects.all():
         if car.brand.company_name in dcars:
             dcars[car.brand.company_name].append(car.name)
         else:
             dcars[car.brand.company_name] = [car.name]
         list_cars.append((car.name,car.name))
 
-    brands = [str(brand) for brand in mdl.Brand.objects.all()]
+    brands = [str(brand) for brand in Brand.objects.all()]
 
     brand_select = forms.ChoiceField(choices=([(brand, brand) for brand in brands]))
     car_select = forms.ChoiceField(choices=(list_cars))
@@ -22,5 +22,5 @@ class RegCarForm(forms.ModelForm):
     cars = json.dumps(dcars)
 
     class Meta:
-        model = mdl.Fleet
+        model = Fleet
         fields = ('brand_select', 'car_select', 'description',)
