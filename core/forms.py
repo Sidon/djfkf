26cc8a3a25b69b856fbc39871a2a8a88d1e9f9b9
sidon <sidoncd@gmail.com>
2017-03-27 17:13:14 -0300

    Second commit

--- core/forms.py
+++ core/forms.py
@@ -1,20 +1,19 @@
 import json
 from django import forms
-from .models import Brand, Car, Fleet
-
+from . import models as mdl
 
 class RegCarForm(forms.ModelForm):
 
     dcars = {}
     list_cars = []
-    for car in Car.objects.all():
+    for car in mdl.Car.objects.all():
         if car.brand.company_name in dcars:
             dcars[car.brand.company_name].append(car.name)
         else:
             dcars[car.brand.company_name] = [car.name]
         list_cars.append((car.name,car.name))
 
-    brands = [str(brand) for brand in Brand.objects.all()]
+    brands = [str(brand) for brand in mdl.Brand.objects.all()]
 
     brand_select = forms.ChoiceField(choices=([(brand, brand) for brand in brands]))
     car_select = forms.ChoiceField(choices=(list_cars))
@@ -23,5 +22,5 @@ class RegCarForm(forms.ModelForm):
     cars = json.dumps(dcars)
 
     class Meta:
-        model = Fleet
+        model = mdl.Fleet
         fields = ('brand_select', 'car_select', 'description',)
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^regcar/$', views.regcar, name='regcar')
